d0d693eaf4aed071383cb30270679c6a08bbe19d
tb1331 <57438922+tb1331@users.noreply.github.com>
2023-12-06 21:36:09 +0700

    Add files via upload

---
+++ js/tab.js
@@ -0,0 +1,10 @@
+function openTab(tabName) {
+  const tabcontent = document.getElementsByClassName("tabcontent");
+  for (let i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  document.getElementById(tabName).style.display = "block";
+}
+
+
+
